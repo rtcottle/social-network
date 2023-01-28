7101edb078ca02c77cf784f30529e8a1c073b89e
Ryan C <111948791+GreyJedi17@users.noreply.github.com>
2023-01-28 10:51:03 -0700

    added user routes and controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -24,6 +24,23 @@ module.exports = {
       .then((user) => res.json(user))
       .catch((err) => res.status(500).json(err));
   },
+  // update existing user
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with this id!' })
+          : res.json(user)
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
   // Delete a user and associated thoughts
   deleteUser(req, res) {
     User.findOneAndDelete({ _id: req.params.userId })
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const thoughtRoutes = require('./thoughtRoutes');
 const reactionRoutes = require('./reactionRoutes');
+const userRoutes = require('./userRoutes');
 
 router.use('/thoughts', thoughtRoutes);
 router.use('/reactions', reactionRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
